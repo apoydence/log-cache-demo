950e9b2628a90eb11ba706f3c2d6e3e136086591
Andrew Poydence <apoydence@pivotal.io>
2018-09-12 20:44:12 -0600

    Improves plotter

--- cmd/promql-plotter/main.go
+++ cmd/promql-plotter/main.go
@@ -1,26 +1,29 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"sort"
+	"strconv"
+	"time"
 
 	envstruct "code.cloudfoundry.org/go-envstruct"
 	"github.com/apoydence/cf-faas-log-cache"
 	"github.com/apoydence/cf-faas-log-cache/pkg/promql"
 	gocapi "github.com/apoydence/go-capi"
 	"github.com/wcharczuk/go-chart"
+	"github.com/wcharczuk/go-chart/drawing"
 )
 
 func main() {
 	log := log.New(os.Stderr, "", 0)
 
-	if len(os.Args) != 2 {
-		log.Fatalf("Usage: %s [PromQL Query]", os.Args[0])
+	if len(os.Args) < 2 {
+		log.Fatalf("Usage: %s [PromQL Queries]", os.Args[0])
 	}
-	query := os.Args[1]
 
 	cfg := LoadConfig(log)
 
@@ -39,87 +42,163 @@ func main() {
 		addTokenDoer{cfg.Token},
 	)
 
-	http.HandleFunc("/", drawChart(query, logCacheClient, cfg, log))
+	http.HandleFunc("/", drawChart(os.Args[1:], logCacheClient, cfg, log))
 	http.HandleFunc("/favicon.ico", func(res http.ResponseWriter, req *http.Request) {
 		res.Write([]byte{})
 	})
 	http.ListenAndServe(fmt.Sprintf(":%d", cfg.Port), nil)
 }
 
-func drawChart(query string, c *promql.Client, cfg Config, log *log.Logger) func(res http.ResponseWriter, req *http.Request) {
-	return func(res http.ResponseWriter, req *http.Request) {
-		result, err := c.PromQL(req.Context(), query)
-		if err != nil {
-			log.Printf("failed to make request: %s", err)
-			res.WriteHeader(http.StatusInternalServerError)
-			res.Write([]byte("failed to make request"))
-			return
-		}
+var colors = []drawing.Color{
+	chart.ColorBlue,
+	chart.ColorRed,
+	chart.ColorGreen,
+	chart.ColorOrange,
+	chart.ColorBlack,
+}
 
-		if result.Status != "success" {
-			log.Printf("failed to make request: invalid Status %s", result.Status)
-			res.WriteHeader(http.StatusInternalServerError)
-			res.Write([]byte("failed to make request"))
-			return
-		}
+var colorMap = map[drawing.Color]int{
+	chart.ColorBlue:   0,
+	chart.ColorRed:    1,
+	chart.ColorGreen:  2,
+	chart.ColorOrange: 3,
+	chart.ColorBlack:  4,
+}
 
-		if result.Data.ResultType != "matrix" {
-			log.Printf("query must yield a matrix")
-			res.WriteHeader(http.StatusInternalServerError)
-			res.Write([]byte("failed to make request"))
-			return
-		}
+func drawChart(
+	queries []string,
+	c *promql.Client,
+	cfg Config,
+	log *log.Logger,
+) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, req *http.Request) {
+		now := time.Now()
 
-		var (
-			// xValues []time.Time
-			xValues []float64
-			yValues []float64
-		)
+		results := make(chan struct {
+			result *faaspromql.QueryResult
+			color  drawing.Color
+		}, len(queries))
 
-		for _, p := range result.Data.Result {
-			s := p.(*faaspromql.Series)
-			for _, v := range s.Values {
-				i, err := v[0].Int64()
+		errs := make(chan error, len(queries))
+
+		var scalars []float64
+		for i, query := range queries {
+			if f, err := strconv.ParseFloat(query, 64); err == nil {
+				scalars = append(scalars, f)
+				continue
+			}
+
+			go func(query string, color drawing.Color) {
+				result, err := c.PromQLRange(req.Context(), query, now.Add(-5*time.Minute), time.Now(), time.Second)
 				if err != nil {
-					log.Panic(err)
+					errs <- fmt.Errorf("failed to make request: %s", err)
+					return
 				}
 
-				t := float64(i / 1e6)
-
-				f, err := v[1].Float64()
-				if err != nil {
-					log.Panic(err)
+				if result.Status != "success" {
+					errs <- fmt.Errorf("failed to make request (invalid status): %s", result.Status)
+					return
 				}
 
-				xValues = append(xValues, t)
-				yValues = append(yValues, f)
+				if result.Data.ResultType != "matrix" {
+					errs <- errors.New("query must yield a matrix")
+					return
+				}
+				results <- struct {
+					result *faaspromql.QueryResult
+					color  drawing.Color
+				}{result, color}
+			}(query, colors[i%len(colors)])
+		}
+
+		var series []chart.Series
+		for i := 0; i < len(queries)-len(scalars); i++ {
+			select {
+			case err := <-errs:
+				w.WriteHeader(http.StatusInternalServerError)
+				w.Write([]byte(err.Error()))
+				log.Println(err.Error())
+				return
+			case result := <-results:
+				var (
+					// xValues []time.Time
+					xValues []float64
+					yValues []float64
+				)
+
+				for _, p := range result.result.Data.Result {
+					s := p.(*faaspromql.Series)
+					for _, v := range s.Values {
+						i, err := v[0].Int64()
+						if err != nil {
+							log.Panic(err)
+						}
+
+						t := float64(i / 1e6)
+
+						f, err := v[1].Float64()
+						if err != nil {
+							log.Panic(err)
+						}
+
+						xValues = append(xValues, t)
+						yValues = append(yValues, f)
+					}
+				}
+
+				sort.Sort(xyPair{xValues, yValues})
+
+				for i := 1; i < len(xValues); i++ {
+					for xValues[i-1] >= xValues[i] {
+						xValues[i] += 50
+					}
+				}
+
+				style := chart.Style{
+					Show:        true,
+					StrokeColor: result.color,
+					FillColor:   result.color.WithAlpha(100),
+				}
+
+				if cfg.Scatter {
+					style = chart.Style{
+						Show:        true,
+						StrokeWidth: chart.Disabled,
+						StrokeColor: result.color,
+						DotWidth:    5,
+					}
+				}
+
+				series = append(series, chart.ContinuousSeries{
+					Style:   style,
+					XValues: xValues,
+					YValues: yValues,
+				})
 			}
 		}
 
-		sort.Sort(xyPair{xValues, yValues})
+		if len(series) == 0 {
+			log.Println("there has to be atleast one series")
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("there has to be atleast one series"))
+			return
+		}
 
-		for i := 1; i < len(xValues); i++ {
-			for xValues[i-1] >= xValues[i] {
-				xValues[i] += 50
+		sort.Sort(seriesColors(series))
+
+		for i, scalar := range scalars {
+			var cs chart.ContinuousSeries
+			for _, x := range series[0].(chart.ContinuousSeries).XValues {
+				cs.XValues = append(cs.XValues, x)
+				cs.YValues = append(cs.YValues, scalar)
 			}
-		}
-
-		if cfg.PrintNumPoints {
-			log.Printf("Number of Points: %d", len(xValues))
-		}
-
-		style := chart.Style{
-			Show:        true,
-			StrokeColor: chart.ColorBlue,
-			FillColor:   chart.ColorBlue.WithAlpha(100),
-		}
-
-		if cfg.Scatter {
-			style = chart.Style{
+			color := colors[(len(series)+i)%len(colors)]
+			cs.Style = chart.Style{
 				Show:        true,
-				StrokeWidth: chart.Disabled,
-				DotWidth:    5,
+				StrokeColor: color,
+				FillColor:   color.WithAlpha(100),
 			}
+			series = append(series, cs)
 		}
 
 		graph := chart.Chart{
@@ -133,28 +212,21 @@ func drawChart(query string, c *promql.Client, cfg Config, log *log.Logger) func
 					Show: true,
 				},
 			},
-			Series: []chart.Series{
-				chart.ContinuousSeries{
-					Style:   style,
-					XValues: xValues,
-					YValues: yValues,
-				},
-			},
+			Series: series,
 		}
 
-		res.Header().Set("Content-Type", "image/png")
-		graph.Render(chart.PNG, res)
+		w.Header().Set("Content-Type", "image/png")
+		graph.Render(chart.PNG, w)
 	}
 }
 
 type Config struct {
-	Port           int    `env:"PORT"`
-	LogCacheAddr   string `env:"LOG_CACHE_ADDR,required"`
-	CAPIAddr       string `env:"CAPI_ADDR,required"`
-	SpaceID        string `env:"SPACE_ID,required"`
-	Token          string `env:"TOKEN,required"`
-	Scatter        bool   `env:"SCATTER_PLOT"`
-	PrintNumPoints bool   `env:"PRINT_NUM_POINTS"`
+	Port         int    `env:"PORT"`
+	LogCacheAddr string `env:"LOG_CACHE_ADDR,required"`
+	CAPIAddr     string `env:"CAPI_ADDR,required"`
+	SpaceID      string `env:"SPACE_ID,required"`
+	Token        string `env:"TOKEN,required"`
+	Scatter      bool   `env:"SCATTER_PLOT"`
 }
 
 func LoadConfig(log *log.Logger) Config {
@@ -179,7 +251,6 @@ func (d addTokenDoer) Do(r *http.Request) (*http.Response, error) {
 }
 
 type xyPair struct {
-	// xValues []time.Time
 	xValues []float64
 	yValues []float64
 }
@@ -201,4 +272,20 @@ func (p xyPair) Swap(i, j int) {
 func (p xyPair) Less(i, j int) bool {
 	// return p.xValues[i].UnixNano() < p.xValues[j].UnixNano()
 	return p.xValues[i] < p.xValues[j]
+}
+
+type seriesColors []chart.Series
+
+func (c seriesColors) Len() int {
+	return len(c)
+}
+
+func (c seriesColors) Swap(i, j int) {
+	tmp := c[i]
+	c[i] = c[j]
+	c[j] = tmp
+}
+
+func (c seriesColors) Less(i, j int) bool {
+	return colorMap[c[i].(chart.ContinuousSeries).Style.StrokeColor] < colorMap[c[j].(chart.ContinuousSeries).Style.StrokeColor]
 }
